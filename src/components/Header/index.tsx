fc45a9946759dd1b9ffc5c8fa87a65a9e330ea40
ygorthiago <ygorthiagoff@outlook.com>
2021-04-24 00:15:52 -0300

    feat: sidebar responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+import { RiMenuLine } from 'react-icons/ri';
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
 import { Logo } from '../Header/Logo';
 import { NotificationsNav } from '../Header/NotificationsNav';
 import { Profile } from '../Header/Profile';
 import { SearchBox } from '../Header/SearchBox';
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,6 +25,19 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        >
+
+        </IconButton>
+      )}
+
       <Logo />
 
       { isWideVersion &&  <SearchBox /> }
