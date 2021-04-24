57de6a12553cf9961d10e5ae404b97d8082e358c
ygorthiago <ygorthiagoff@outlook.com>
2021-04-23 23:54:46 -0300

    feat: header responsiveness

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Ygor Thiago</Text>
-        <Text color="gray.300" fontSize="small">ygorthiagof@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Ygor Thiago</Text>
+          <Text color="gray.300" fontSize="small">ygorthiagof@gmail.com</Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Ygor Thiago" src="https://github.com/ygorthiago.png" />
     </Flex>
