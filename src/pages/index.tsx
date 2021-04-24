acd579c67572cfe3b84975e8c907b063942a8ab4
ygorthiago <ygorthiagoff@outlook.com>
2021-04-24 17:26:42 -0300

    feat: update required message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,8 @@ type SignInFormData = {
 }
 
 const signInFormSchema = yup.object().shape({
-  email: yup.string().required('E-mail required').email('Invalid e-mail'),
-  password: yup.string().required('Password required'),
+  email: yup.string().required('E-mail is required').email('Invalid e-mail'),
+  password: yup.string().required('Password is required'),
 });
 
 export default function SignIn() {
