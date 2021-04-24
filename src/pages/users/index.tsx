6f0cf49dbe12e20a0f88370ff0826326f086f3f9
ygorthiago <ygorthiagoff@outlook.com>
2021-04-24 01:16:48 -0300

    feat: users list responsiveness

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,13 +10,19 @@ import { Box,
   Td,
   Th,
   Thead,
-  Tr } from "@chakra-ui/react";
+  Tr, 
+  useBreakpointValue} from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -33,89 +39,85 @@ export default function UserList() {
             </Button>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>User</Th>
-                <Th>Register date</Th>
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">
-                      Ygor Thiago
-                    </Text>
-                    <Text fontSize="sm" color="gray.300">
-                      ygorthiagof@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">
-                      Ygor Thiago
-                    </Text>
-                    <Text fontSize="sm" color="gray.300">
-                      ygorthiagof@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">
-                      Ygor Thiago
-                    </Text>
-                    <Text fontSize="sm" color="gray.300">
-                      ygorthiagof@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
-                <Td>
-                  <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
-                    Edit
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          <Box width={300} overflowX="scroll">
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>User</Th>
+                  <Th>Register date</Th>
+                  <Th width="8"></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">
+                        Ygor Thiago
+                      </Text>
+                      <Text fontSize="sm" color="gray.300">
+                        ygorthiagof@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>04 de Abril, 2021</Td> 
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Edit
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">
+                        Ygor Thiago
+                      </Text>
+                      <Text fontSize="sm" color="gray.300">
+                        ygorthiagof@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>04 de Abril, 2021</Td> 
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Edit
+                    </Button>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td px={["4", "4", "6"]}>
+                    <Checkbox colorScheme="pink" />
+                  </Td>
+                  <Td>
+                    <Box>
+                      <Text fontWeight="bold">
+                        Ygor Thiago
+                      </Text>
+                      <Text fontSize="sm" color="gray.300">
+                        ygorthiagof@gmail.com
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>04 de Abril, 2021</Td> 
+                  <Td>
+                    <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>
+                      Edit
+                    </Button>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </Box>
 
           <Pagination />
         </Box>
