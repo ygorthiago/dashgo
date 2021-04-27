e707abe9e292030af633f43370aea554d546bb1a
ygorthiago <ygorthiagoff@outlook.com>
2021-04-27 00:34:51 -0300

    feat: useMutation on create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,7 +6,10 @@ import { Sidebar } from "../../components/Sidebar";
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-
+import { useMutation } from 'react-query';
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/dist/client/router";
 
 type CreateUserFormData = {
   name: string;
@@ -24,14 +27,31 @@ const createUserFormSchema = yup.object().shape({
 
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      },
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    },
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema),
   });
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve=> setTimeout(resolve, 2000));
+    await createUser.mutateAsync(values);
     
-    console.log(values);
+    router.push('/users');
   }
 
   return (
